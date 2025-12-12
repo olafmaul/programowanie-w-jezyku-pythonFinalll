7d2400ee0fcaa9f588a413c375af4c33d6e4ece2
Olaf Maul <olaf.maul@edu.uekat.pl>
2025-12-12 20:36:38 +0100

    zad_11

---
+++ zad_11.py
@@ -0,0 +1,7 @@
+def imie_nazwisko(name,surname):
+    return (f'Cześć{name}{surname}!')
+
+name = 'Olaf'
+surname = 'Maul'
+
+print(imie_nazwisko)
--- zad_5.py
+++ zad_5.py
@@ -4,7 +4,7 @@
 def list_checker(zbior_zakres:list, szukana_liczba:int):
 	return szukana_liczba in zbior_zakres
 
-uno_lista_zakres = [10,20,30,69,99]
+uno_lista_zakres = [10,20,30,69,99,100]
 due_liczba = 100
 
 czy_jest = list_checker(uno_lista_zakres,due_liczba)
