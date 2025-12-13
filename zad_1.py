42d9e476d6771a281e6df6144f41176f8f91f410
Olaf Maul <olaf.maul@edu.uekat.pl>
2025-12-13 12:54:42 +0100

    total

--- zad_1.py
+++ zad_1.py
@@ -1,5 +1,22 @@
-#%%
-names = ['Martin', 'Tom', 'Eva']
+class Student:
+   def __init__(self, name, marks):
+       # Inicjalizacja atrybutów name (imię) i marks (lista ocen)
+       self.name = name
+       self.marks = marks
 
-for name in names:
-    print(name)
+
+   def is_passed(self):
+       if not self.marks:
+           return False
+
+
+       average = sum(self.marks) / len(self.marks)
+       return average > 50
+
+
+student1 = Student("Maria Wolska", [22, 33, 80, 90])
+print(f"Student {student1.name} zdał? {student1.is_passed()}")
+
+
+student2 = Student("Mariusz Kanol", [22, 14, 65, 40])
+print(f"Student {student2.name} zdał? {student2.is_passed()}")
