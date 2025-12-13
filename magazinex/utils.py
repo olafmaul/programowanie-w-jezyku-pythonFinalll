c4047e2e6a37f99fb306a96ac4d68c2680849cbb
Olaf Maul <olaf.maul@edu.uekat.pl>
2025-12-13 15:15:37 +0100

    utils

--- magazinex/utils.py
+++ magazinex/utils.py
@@ -1,5 +1,15 @@
 def greet():
-    return "Hi there, it is utils"
+    return (f"Hi,\n"
+            f"this is your final price after the discount (of {percentage}%):"
+            f"\n")
 
 
-print(greet())
+def calculate_discount(price, percentage):
+    return price * (1 - percentage / 100)
+
+price = 207652
+percentage = 20
+
+after_discount = calculate_discount(price, percentage)
+
+print(greet(),after_discount)
