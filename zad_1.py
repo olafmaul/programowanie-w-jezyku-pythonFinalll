a08b71254db6415aaf7bc34e22bb6000c1fe2b52
Olaf Maul <olaf.maul@edu.uekat.pl>
2025-12-13 14:58:20 +0100

    formatting

--- zad_1.py
+++ zad_1.py
@@ -1,4 +1,3 @@
-#Utwórz funkcję, która otrzyma w parametrze listę 5 imion, a następnie wyświetli każde z nich.
 names = ['Olaf', 'Tomasz', 'Andrzej', 'Marian', 'Slawomir']
 for name in names:
     print(name)
--- zad_2.py
+++ zad_2.py
@@ -1,5 +1,3 @@
-#Utwórz funkcję, która otrzyma w parametrze listę zawierającą 5 dowolnych liczb, każdy jej element pomnoży przez 2, a na końcu ją zwróci. Zadanie należy wykonać w 2 wersjach:
-#Wykorzystując pętle for.
 """ v1
 numbers = [12,32,11,73,8]
 for number in numbers:
@@ -14,7 +12,6 @@ for number in numbers:
 print(numbers_2)
 """
 
-#Wykorzystując listę składaną.
 """
 numbers = [12,32,11,73,8]
 numbers_2= [(number)*2 for number in numbers]
